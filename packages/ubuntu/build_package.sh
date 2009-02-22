2906033d81e84a047efa102d6fb3192c39abd276
Bruce van der Kooij <brucevdkooij@gmail.com>
2009-02-22 00:28:53 +0000

    Corrected some errors lintian threw and added a VERSION file which the build_package scripts reads so we have somewhat of a central VERSION file.

--- packages/ubuntu/build_package.sh
+++ packages/ubuntu/build_package.sh
@@ -3,12 +3,14 @@
 # FIXME: this is obviously just an example of the procedure, still need to actually
 # write it properly (get the version number from somewhere).
 
+package=`head -n 1 VERSION`
+version=`head -n 2 VERSION | tail -n 1`
 rm -rf /tmp/nautilussvn_build
 mkdir /tmp/nautilussvn_build
-svn export . /tmp/nautilussvn_build/nautilussvn-0.12
+svn export . /tmp/nautilussvn_build/${package}-${version}
 cd /tmp/nautilussvn_build
-tar -zcvf nautilussvn_0.12.orig.tar.gz nautilussvn-0.12
-cd nautilussvn-0.12/
+tar -zcvf ${package}_${version}.orig.tar.gz ${package}-${version}
+cd ${package}-${version}/
 cd packages/ubuntu/
 debchange -i
 cd ../../
