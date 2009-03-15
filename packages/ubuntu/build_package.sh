08b6f3c1406c3542adf6f43677142755ab901bad
Bruce van der Kooij <brucevdkooij@gmail.com>
2009-03-15 23:12:11 +0000

    Updated the build script to copy the changelog back into the original directory (which makes sense if it is trunk, but probably less so if not). The point is that the changelog should be comitted.

--- packages/ubuntu/build_package.sh
+++ packages/ubuntu/build_package.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 
-# FIXME: this is obviously just an example of the procedure, still need to actually
-# write it properly (get the version number from somewhere).
+# TODO: this probably needs some work
 
+# This is needed to copy the changelog back later
+original_dir="`cwd`"
+
+# Grab some variables from the VERSION file
 package=`head -n 1 VERSION`
 version=`head -n 2 VERSION | tail -n 1`
+
+# Cleanup
 rm -rf /tmp/nautilussvn_build
 mkdir /tmp/nautilussvn_build
+
+# Export
 svn export . /tmp/nautilussvn_build/${package}-${version}
+
+# Zip up the original source code
 cd /tmp/nautilussvn_build
 tar -zcvf ${package}_${version}.orig.tar.gz ${package}-${version}
 cd ${package}-${version}/
+
+# Update the changelog (be sure to commit this afterwards)
 cd packages/ubuntu/
 debchange -i
+cp ./debian/changelog "${original_dir}/packages/ubuntu/debian"
+
+# Copy the Debian directory into place and build the directory
 cd ../../
 cp -R packages/ubuntu/debian/ .
 debuild
+
+# Let the user know he should commit
+echo ""
+echo "================================================================="
+echo " PLEASE COMMIT THE NEW CHANGELOG"
+echo "================================================================="
+echo ""
