9dc51fc1dedd48073aef35e781aa914eaee877b9
Jason Heeris <jason.heeris@gmail.com>
2009-10-03 15:21:54 +0000

    Updated packaging scripts.

--- packages/debian.py
+++ packages/debian.py
@@ -1,9 +1,8 @@
-import os, os.path, shutil, subprocess
-import pysvn
+from generic import Generic
 
-PACKAGE_FILES_SUBDIR = "packages"
+import shutil, subprocess, os.path
 
-class Debian:
+class Debian(Generic):
     """ A builder for all Debian-like distributions.
     """   
     
@@ -12,68 +11,27 @@ class Debian:
 
     control_dir = "debian"
     
-    def __init__(self, working_copy, build_area, output_dir, distro, name, version):
-        self.working_copy = working_copy
-        self.build_area = build_area
-        self.output_dir = output_dir
-        self.distro = distro
-
-
-        self.name = name
-        self.version = version
-        self.package_dir_rel = self.name + "-" + self.version        
-        self.package_dir = os.path.join(build_area, self.package_dir_rel)
+    def __init__(self, *args, **kwargs):
+        Generic.__init__(self, *args, **kwargs)
         self.orig_ark = None
-
-    def _copy_source(self):               
-        try:
-            svn_client = pysvn.Client()
-            svn_client.export(self.working_copy, self.package_dir)
-            print "Exported SVN working copy."
-        except pysvn.ClientError, e:
-            shutil.copytree(self.working_copy, self.package_dir)
-            print "Copied file tree."
-        
-    def _compress_orig(self):
-        ark_name = self.name + "_" + self.version + ".orig.tar.gz"
-        ark_path = os.path.join(self.build_area, ark_name)
-
-        print "Creating orig archive... ",
-
-        retval = subprocess.Popen(
-                    ["tar", "-czf", ark_path, self.package_dir_rel],
-                    cwd = self.build_area).wait()
-
-        # This doesn't work: uses absolute paths unless we chdir
-        # ark = tarfile.open(
-        #         os.path.join(self.build_area, ark_name),
-        #         mode = "w:gz")
-        # ark.add(self.package_dir)
-        # ark.close()
-
-        if retval == 0:
-            print "Done."
-        else:
-            print "Failed!"
-            raise RuntimeException("Failed to create orig archive!")
-
-        assert os.path.isfile(ark_path)
-        
-        self.orig_ark = ark_path
-        
-        print "Archive created: %s" % ark_path
-    
+        self.orig_ark_name = self.name + "_" + self.version + ".orig.tar.gz"
+       
     def _debianise(self):
         print "Debianising source... ",
         
         control_dir = os.path.join(self.package_dir,
-                                   PACKAGE_FILES_SUBDIR,
+                                   Generic.PACKAGE_FILES_SUBDIR,
                                    self.distro,
                                    Debian.control_dir)
                                    
         shutil.copytree(control_dir,
                         os.path.join(self.package_dir,
                                      Debian.control_dir))
+    
+    def _common_prebuild(self):
+        self._copy_source()
+        self.orig_ark = self._compress(self.orig_ark_name)
+        self._debianise()
 
     def _build_source(self):
         print "Running dpkg-source to create Debian source package..."
@@ -88,21 +46,12 @@ class Debian:
         retval = subprocess.Popen(
             ["debuild", "-us", "-uc", "-b"],
             cwd = self.package_dir).wait()
-    
-    def _copy_results(self):
-        # List all non-directory files
-        files = os.walk(self.build_area).next()[2]
-        [shutil.copy(
-            os.path.join(self.build_area, file),
-            self.output_dir)                        for file in files]
         
     def build_current_source(self):
         """ Builds a Debian (like) source package from the current state of the
         tree.
         """
-        self._copy_source()
-        self._compress_orig()
-        self._debianise()
+        self._common_prebuild()
         self._build_source()
         self._copy_results()
         
@@ -110,8 +59,6 @@ class Debian:
         """ Builds a Debian (like) binary package from the current state of the
         tree.
         """
-        self._copy_source()
-        self._compress_orig()
-        self._debianise()
+        self._common_prebuild()
         self._build_binary()
-        self._copy_results()
+        self._copy_results("*.deb")
---
+++ packages/generic.py
@@ -0,0 +1,73 @@
+import os, os.path, shutil, subprocess, fnmatch
+import pysvn
+
+class Generic(object):
+
+    PACKAGE_FILES_SUBDIR = "packages"
+
+    distros = ["generic"]
+    
+    def __init__(self, working_copy, build_area, output_dir, distro, name, version):
+        self.working_copy = working_copy
+        self.build_area = build_area
+        self.output_dir = output_dir
+        self.distro = distro
+        self.name = name
+        self.version = version
+        self.package_dir_rel = self.name + "-" + self.version        
+        self.package_dir = os.path.join(build_area, self.package_dir_rel)
+
+    def _copy_source(self):               
+        try:
+            svn_client = pysvn.Client()
+            svn_client.export(self.working_copy, self.package_dir)
+            print "Exported SVN working copy."
+        except pysvn.ClientError:
+            shutil.copytree(self.working_copy, self.package_dir)
+            print "Copied file tree."
+
+    def _compress(self, ark_name = None):
+        if not ark_name:
+            ark_name = self.name + "_" + self.version + ".tar.gz"
+        
+        ark_path = os.path.join(self.build_area, ark_name)
+
+        print "Creating orig archive... ",
+
+        retval = subprocess.Popen(
+                    ["tar", "-czf", ark_path, self.package_dir_rel],
+                    cwd = self.build_area).wait()
+
+        # This doesn't work: uses absolute paths unless we chdir
+        # ark = tarfile.open(
+        #         os.path.join(self.build_area, ark_name),
+        #         mode = "w:gz")
+        # ark.add(self.package_dir)
+        # ark.close()
+
+        if retval == 0:
+            print "Done."
+        else:
+            print "Failed!"
+            raise RuntimeError("Failed to create orig archive!")
+
+        assert os.path.isfile(ark_path)
+        
+        print "Archive created: %s" % ark_path
+        
+        return ark_path
+
+    def _copy_results(self, match = "*"):
+        # List all non-directory files
+        files = fnmatch.filter(os.walk(self.build_area).next()[2], match)
+        
+        [shutil.copy(
+            os.path.join(self.build_area, fl),
+            # fl,
+            self.output_dir) \
+                for fl in files]
+
+    def build_current_source(self):
+        self._copy_source()
+        self._compress()
+        self._copy_results("*.tar.gz")
