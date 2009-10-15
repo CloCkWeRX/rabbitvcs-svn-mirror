8cf0d13504658ada28d7c7b67d94c029b37bbbf9
Jason Heeris <jason.heeris@gmail.com>
2009-10-15 15:33:33 +0000

    This should be a slightly better way of sorting out localisation problems.

--- rabbitvcs/util/locale.py
+++ rabbitvcs/util/locale.py
@@ -1,12 +1,28 @@
 from __future__ import absolute_import 
 
 import locale
+import os
+
+from rabbitvcs.lib.log import Log
+log = Log("rabbitvcs.util.locale")
 
 def initialize_locale():
-    _locale, encoding = locale.getdefaultlocale()
-    if _locale is None:
-        _locale = "en_US"
-    if encoding is None:
-        encoding = "utf8"
+    try:
+        # Just try to set the default locale for the user
+        locale.resetlocale(locale.LC_ALL)
+    except locale.Error:
+        # If the user's environment does not specify an encoding, Python will
+        # pick a default which might not be available. It seems to pick
+        # ISO8859-1 (latin1), but UTF8 is a better idea on GNU/Linux.
+        log.warning("Could not set default locale (LANG: %s)" % os.environ.get("LANG"))
         
-    locale.setlocale(locale.LC_ALL, (_locale, encoding))
+        (loc, enc) = locale.getdefaultlocale()
+        
+        # We should only try this if we have a region to set as well.
+        if loc and enc != "UTF8":
+            try:
+                locale.setlocale(locale.LC_ALL, (loc, "UTF8"))
+                log.warning("Manually set encoding to UTF-8")
+            except locale.Error:
+                # Nope, no UTF8 either.
+                log.warning("Could not set user's locale to UTF-8")
