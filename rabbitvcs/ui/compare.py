4c6bb5d9714a6544dd19adc1810023e1af28ed83
Adam Plumb <adamplumb@gmail.com>
2009-10-19 15:49:26 +0000

    Connected and implemented most signals in the UI.  Some further refinement of the ui

--- rabbitvcs/ui/compare.py
+++ rabbitvcs/ui/compare.py
@@ -25,12 +25,40 @@ import gobject
 import gtk
 
 from rabbitvcs.ui import InterfaceView
+import rabbitvcs.ui.widget
+import rabbitvcs.lib.helper
+from rabbitvcs.ui.log import LogDialog
 from rabbitvcs import gettext
 _ = gettext.gettext
 
 class Compare(InterfaceView):
     def __init__(self, path1=None, revision1=None, path2=None, revision2=None):
         InterfaceView.__init__(self, "compare", "Compare")
+
+        repo_paths = rabbitvcs.lib.helper.get_repository_paths()
+        self.first_urls = rabbitvcs.ui.widget.ComboBox(
+            self.get_widget("first_urls"), 
+            repo_paths
+        )
+        self.first_urls_browse = self.get_widget("first_urls_browse")
+
+        self.second_urls = rabbitvcs.ui.widget.ComboBox(
+            self.get_widget("second_urls"), 
+            repo_paths
+        )
+        self.second_urls_browse = self.get_widget("second_urls_browse")
+        
+        self.first_revision_opt = self.get_widget("first_revision_opt")
+        self.first_revision_number = self.get_widget("first_revision_number")
+        self.first_revision_browse = self.get_widget("first_revision_browse")
+        self.first_revision_opt.set_active(0)
+        
+        self.second_revision_opt = self.get_widget("second_revision_opt")
+        self.second_revision_number = self.get_widget("second_revision_number")
+        self.second_revision_browse = self.get_widget("second_revision_browse")
+        self.second_revision_opt.set_active(0)
+        
+        self.check_ui()
 
     #
     # UI Signal Callback Methods
@@ -41,6 +69,82 @@ class Compare(InterfaceView):
 
     def on_close_clicked(self, widget):
         self.close()
+    
+    def on_first_urls_changed(self, widget, data=None):
+        self.check_first_urls()
+
+    def on_second_urls_changed(self, widget, data=None):
+        self.check_second_urls()
+   
+    def on_first_revision_opt_changed(self, widget, data=None):
+        self.check_first_revision()
+
+    def on_second_revision_opt_changed(self, widget, data=None):
+        self.check_second_revision()
+
+    def on_first_urls_browse_clicked(self, widget, data=None):
+        rabbitvcs.lib.helper.launch_repo_browser(
+            self.first_urls.get_active_text()
+        )
+
+    def on_second_urls_browse_clicked(self, widget, data=None):
+        rabbitvcs.lib.helper.launch_repo_browser(
+            self.second_urls.get_active_text()
+        )
+
+    def on_first_revision_browse_clicked(self, widget, data=None):
+        LogDialog(
+            self.first_urls.get_active_text(), 
+            ok_callback=self.on_first_log_closed
+        )
+
+    def on_first_log_closed(self, data):
+        if data is not None:
+            self.first_revision_opt.set_active(1)
+            self.first_revision_number.set_text(data)
+
+    def on_second_revision_browse_clicked(self, widget, data=None):
+        LogDialog(
+            self.second_urls.get_active_text(), 
+            ok_callback=self.on_second_log_closed
+        )
+
+    def on_second_log_closed(self, data):
+        if data is not None:
+            self.second_revision_opt.set_active(1)
+            self.second_revision_number.set_text(data)
+
+    #
+    # Helper methods
+    #
+    
+    def check_ui(self):
+        self.check_first_urls()
+        self.check_second_urls()
+        self.check_first_revision()
+        self.check_second_revision()
+    
+    def check_first_urls(self):
+        status = (self.first_urls.get_active_text() != "")
+        
+        self.first_revision_browse.set_sensitive(status)
+        self.first_urls_browse.set_sensitive(status)
+        
+    def check_second_urls(self):
+        status = (self.second_urls.get_active_text() != "")
+        
+        self.second_revision_browse.set_sensitive(status)
+        self.second_urls_browse.set_sensitive(status)
+        
+    def check_first_revision(self):
+        sensitive = (self.first_revision_opt.get_active() == 1)
+        self.first_revision_number.set_sensitive(sensitive)
+        self.first_revision_browse.set_sensitive(sensitive)
+
+    def check_second_revision(self):
+        sensitive = (self.second_revision_opt.get_active() == 1)
+        self.second_revision_number.set_sensitive(sensitive)
+        self.second_revision_browse.set_sensitive(sensitive)
 
 def parse_path_revision_string(pathrev):
     index = pathrev.rfind("@")
