d53b1517234970d3a06d39eaa47ba531a4ca14d4
Jason Heeris <jason.heeris@gmail.com>
2009-11-08 12:53:50 +0000

    Added (but commented) an extra status checker implementation line.

--- rabbitvcs/services/statuscache.py
+++ rabbitvcs/services/statuscache.py
@@ -36,6 +36,7 @@ from Queue import Queue
 
 # from rabbitvcs.services.checkerservice import StatusCheckerStub as StatusChecker
 # from rabbitvcs.services.simplechecker import StatusChecker
+# from rabbitvcs.services.statuschecker import StatusChecker
 from rabbitvcs.services.loopedchecker import StatusChecker
 
 import rabbitvcs.util.vcs
