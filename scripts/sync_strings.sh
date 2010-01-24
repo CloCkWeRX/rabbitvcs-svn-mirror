14160e3b536fb0ac7b53468059ebe03169b32fd4
Adam Plumb <adamplumb@gmail.com>
2010-01-24 17:05:04 +0000

    Renamed translate.sh to a more descriptive and precise name

--- scripts/sync_strings.sh
+++ scripts/sync_strings.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Set up a translation file
-# 1. Extract strings from glade files into .h fileS
-# 2. Extract gettext strings from py files
-# 3. Delete the glade .h files
+# Sync our translation template file with the actual strings
+# 1. Extracts strings from glade files into .h fileS
+# 2. Extracts gettext strings from py files
+# 3. Deletes the glade .h files
 
-cd rabbitvcs
+cd ../rabbitvcs
 
 for i in `find . | grep '\.glade' | grep -v '\.svn'`;
 do
