7f0b7b039ef20fbf9482429ef245295ba3584853
Jason Heeris <jason.heeris@gmail.com>
2010-03-17 14:42:00 +0000

    Added SVN status object.

--- rabbitvcs/vcs/status.py
+++ rabbitvcs/vcs/status.py
@@ -1,3 +1,6 @@
+from rabbitvcs import gettext
+_ = gettext.gettext
+
 # These are the statuses that we might represent with icons
 status_unchanged = 'unchanged'
 status_changed = 'changed'
@@ -11,18 +14,19 @@ status_complicated = 'complicated'
 status_calculating = 'calculating'
 status_error = 'error'
  
-class GenericStatus:
+class GenericStatus(object):
+ 
+    vcs_type = 'generic'
  
     clean_statuses = ['unchanged']
  
     content_status_map = None
     metadata_status_map = None
  
-    def __init__(self, content_status, metadata_status, vcs_type = None):
+    def __init__(self, content_status, metadata_status):
         # vcs_type may be None for things like error, calculating, etc
-        self.vcs = vcs_type
-        self.content = vcs_content_status
-        self.metadata = vcs_metadata_status
+        self.content = content_status
+        self.metadata = metadata_status
         self.single = self._make_single_status()
  
     def _make_single_status(self):
@@ -43,3 +47,21 @@ class GenericStatus:
     def simple_metadata_status(self):
         if self.metadata_status_map:
             return self.metadata_status_map.get(self.metadata)
+        
+    def __repr__(self):
+        return "<%s (%s) %s/%s>" % (_("RabbitVCS status"),
+                                    self.vcs_type,
+                                    self.content,
+                                    self.metadata)
+
+class SVNStatus(GenericStatus):
+    
+    vcs_type = 'subversion'
+    
+    def __init__(self, pysvn_status):
+        # There is a potential problem here: I'm pretty sure that PySVN statuses
+        # do NOT have translatable representations, so this will always come out
+        # to be 'normal', 'modified' etc
+        super(SVNStatus, self).__init__(
+            content_status=str(pysvn_status.text_status),
+            metadata_status=str(pysvn_status.prop_status))
