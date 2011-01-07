188be594fc61bc86a7dfa3c02892fbafa823896a
Adam Plumb <adamplumb@gmail.com>
2011-01-07 17:20:04 +0000

    v0.14.1 Release

--- rabbitvcs/__init__.py
+++ rabbitvcs/__init__.py
@@ -31,7 +31,7 @@ try:
 except Exception, e:
     pass
 
-version = "0.14"
+version = "0.14.1"
 APP_NAME = "RabbitVCS"
 TEMP_DIR_PREFIX = "rabbitvcs-"
 LOCALE_DIR = "%s/locale" % os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
