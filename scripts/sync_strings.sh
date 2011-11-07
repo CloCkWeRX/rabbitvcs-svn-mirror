4fc0f6694698524ddc8e65f1b9cb75ad72cfebcb
Adam Plumb <adamplumb@gmail.com>
2011-11-07 19:04:11 +0000

    Synchronize pot file and update the script to be used with gtkbuilder files

--- scripts/sync_strings.sh
+++ scripts/sync_strings.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Sync our translation template file with the actual strings
-# 1. Extracts strings from glade files into .h fileS
+# 1. Extracts strings from gtkbuilder xml files into .h files
 # 2. Extracts gettext strings from py files
 # 3. Deletes the glade .h files
 
 cd ..
 
-for i in `find . | grep '\.glade' | grep -v '\.svn'`;
+for i in `find . | grep '\.xml' | grep -v '\.svn' | grep -v '\.xml\.h'`;
 do
 	intltool-extract --type=gettext/glade $i
 done
@@ -15,16 +15,15 @@ done
 cd rabbitvcs
 
 echo "util/helper.py" > POTFILES.in
-find . -type f | grep ui | grep \.py | grep -v \.svn | grep -v \.pyc >> POTFILES.in 
-find . -type f | grep \.glade | grep -v \.svn | grep -v \.h >> POTFILES.in
-sed -i 's|\.glade|.glade.h|g' POTFILES.in
+find . -type f | egrep '(ui|.xml)' | grep -v \.svn | grep -v \.h | grep -v \.pyc >> POTFILES.in 
+sed -i 's|\.xml|.xml.h|g' POTFILES.in
 sed -i 's|\.\/||g' POTFILES.in
 
 mv POTFILES.in ../po/POTFILES.in
 
 xgettext -L Python --keyword=_ --keyword=N_ -o ../po/RabbitVCS.pot -f ../po/POTFILES.in
 
-for i in `find . | grep '\.glade\.h' | grep -v '\.svn'`;
+for i in `find . | grep '\.xml\.h' | grep -v '\.svn'`;
 do
 	rm -f $i
 done
