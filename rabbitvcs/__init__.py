c7f6ce4bf070fee4c2682b7a406254aae2d3b78e
Kory Becker <kbecker@primaryobjects.com>
2014-08-22 17:08:30 -0500

    Updated version to 0.16.1, includes password prompt.

--- rabbitvcs/__init__.py
+++ rabbitvcs/__init__.py
@@ -31,7 +31,7 @@ try:
 except Exception, e:
     pass
 
-version = "0.16.0"
+version = "0.16.1"
 APP_NAME = "RabbitVCS"
 TEMP_DIR_PREFIX = "rabbitvcs-"
 LOCALE_DIR = "%s/locale" % os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
